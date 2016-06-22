e17a83234afa3e113632e98b63293c6522cba2a6
bonus630 <bonus630@gmail.com>
2016-06-22 15:37:31 -0300

    First commit

---
+++ ExtensionMethods/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ExtensionMethods
+{
+    public static class Extension
+    {
+        public static System.Windows.Media.SolidColorBrush ToSystemColor(this VGCore.Color corelColor)
+        {
+            string hexValue = corelColor.HexValue;
+            System.Windows.Media.Color color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(hexValue);
+            return new System.Windows.Media.SolidColorBrush(color);
+            
+        }
+        public static System.Windows.Media.Imaging.BitmapSource genereteBitmapSource(this System.Drawing.Bitmap resource)
+        {
+            var bitmap = new System.Drawing.Bitmap(resource);
+            var bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                                                                                  IntPtr.Zero,
+                                                                                  System.Windows.Int32Rect.Empty,
+                                                                                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions()
+                  );
+
+            bitmap.Dispose();
+            return bitmapSource;
+        }
+    }
+}
