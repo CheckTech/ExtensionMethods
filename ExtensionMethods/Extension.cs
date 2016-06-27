cd97a3c243a517e8852285ef81a4e7500a8a2f8d
bonus630 <bonus630@gmail.com>
2016-06-27 17:53:36 -0300

    Add method who Converts 24bpp format to 32bpp

--- ExtensionMethods/Extension.cs
+++ ExtensionMethods/Extension.cs
@@ -26,5 +26,35 @@ namespace ExtensionMethods
             bitmap.Dispose();
             return bitmapSource;
         }
+        ///Converts 24bpp format to 32bpp
+        public static System.Windows.Media.Imaging.BitmapSource GetImageFromClipboard()
+        {
+            if (System.Windows.Clipboard.GetDataObject() == null) return null;
+            if (System.Windows.Clipboard.GetDataObject().GetDataPresent(System.Windows.DataFormats.Dib))
+            {
+                var dib = ((System.IO.MemoryStream)System.Windows.Clipboard.GetData(System.Windows.DataFormats.Dib)).ToArray();
+                var width = BitConverter.ToInt32(dib, 4);
+                var height = BitConverter.ToInt32(dib, 8);
+                var bpp = BitConverter.ToInt16(dib, 14);
+                if (bpp == 32)
+                {
+                    var gch = System.Runtime.InteropServices.GCHandle.Alloc(dib, System.Runtime.InteropServices.GCHandleType.Pinned);
+                    System.Drawing.Bitmap bmp = null;
+                    try
+                    {
+                        var ptr = new IntPtr((long)gch.AddrOfPinnedObject() + 40);
+                        bmp = new System.Drawing.Bitmap(width, height, width * 4, System.Drawing.Imaging.PixelFormat.Format32bppArgb, ptr);
+                        return new System.Drawing.Bitmap(bmp).genereteBitmapSource();
+                    }
+                    finally
+                    {
+                        gch.Free();
+                        if (bmp != null) bmp.Dispose();
+                    }
+                }
+            }
+            
+            return System.Windows.Clipboard.ContainsImage() ? System.Windows.Clipboard.GetImage() : null;
+        }
     }
 }
